749168c98df5d0c89ad406cc26f9e215198a8f4a
vaibhav Bansode <vaibhavbansode855@gmail.com>
2022-11-02 17:44:07 +0530

    project complete

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from "./logo.svg";
+import "./App.css";
+
+import Board from "./tictactoeGame/Board";
+
+function App() {
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  );
+}
+
+export default App;
